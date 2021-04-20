21e0513ae7341bfbb2be20da67c13a5f8b9e8c6b
oscarredondo29@gmail.com <oscarredondo29@gmail.com>
2021-04-20 18:15:34 +0200

    ultima actualiacion del pop espero

--- src/main/java/cat/itb/m9/springboot/exercicis/ex05formularis_lombok/seguretat/ConfiguracioSeguretatWeb.java
+++ src/main/java/cat/itb/m9/springboot/exercicis/ex05formularis_lombok/seguretat/ConfiguracioSeguretatWeb.java
@@ -33,7 +33,7 @@ public class ConfiguracioSeguretatWeb extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","/login","/register/**","/error","/", "/h2-console/**", "/registre").permitAll()
+                .antMatchers("/","/login","/register/**","/error","/",  "/registre").permitAll()
                 .antMatchers("/empleats/edit/**","/empleats/new/submit","/empleats/modificar/**","/empleats/list").authenticated()
                 .antMatchers("/**").hasRole("ADMIN")
                 .and()
@@ -44,8 +44,8 @@ public class ConfiguracioSeguretatWeb extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutSuccessUrl("/")
                 .permitAll();
-                http.csrf().disable(); //per h2-console
-                http.headers().frameOptions().disable();
+//                http.csrf().disable(); //per h2-console
+//                http.headers().frameOptions().disable();
 
                 //todo conraul no puedo hacer  cosas
 
